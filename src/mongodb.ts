99f0ab953af8ed0d4c13b29f22ad233ad594f5c0
mgarnier11 <mgarnier11@gmail.com>
2019-10-15 21:24:52 +0200

    Fix: Deployement

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -4,14 +4,19 @@ import { MongoClient } from 'mongodb';
 import { Application } from './declarations';
 const logger = require('./logger');
 
-export default function (app: Application) {
+export default function(app: Application) {
   const config = app.get('mongodb');
-  const promise = MongoClient.connect(config, { useNewUrlParser: true }).then(client => {
-    const dbName = parse(config, () => {});
-    return client.db(dbName);
-  }).catch(error => {
-    logger.error(error);
-  });
+  const promise = MongoClient.connect(config, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+    .then(client => {
+      const dbName = parse(config, () => {});
+      return client.db(dbName);
+    })
+    .catch(error => {
+      logger.error(error);
+    });
 
   app.set('mongoClient', promise);
 }
