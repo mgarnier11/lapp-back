df470fe1332b5c95c8d251929568f17eef02d789
mgarnier11 <mgarnier11@gmail.com>
2019-12-21 00:05:35 +0100

    dev en cours

--- src/services/games/games.service.ts
+++ src/services/games/games.service.ts
@@ -1,28 +1,29 @@
 // Initializes the `games` service on path `/games`
-import { ServiceAddons } from '@feathersjs/feathers';
-import { Application } from '../../declarations';
-import { GameServiceClass } from './games.class';
-import hooks from './games.hooks';
+import { ServiceAddons } from "@feathersjs/feathers";
+import { Application } from "../../declarations";
+import { GameServiceClass } from "./games.class";
+import hooks from "./games.hooks";
 
 // Add this service to the service type index
-declare module '../../declarations' {
+declare module "../../declarations" {
   interface ServiceTypes {
     games: GameServiceClass & ServiceAddons<any>;
   }
 }
 
 export default function(app: Application) {
-  const paginate = app.get('paginate');
+  const paginate = app.get("paginate");
 
   const options = {
-    paginate: false
+    paginate: false,
+    whitelist: ["$elemMatch"]
   };
 
   // Initialize our service with any options it requires
-  app.use('/games', new GameServiceClass(options, app));
+  app.use("/games", new GameServiceClass(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('games');
+  const service = app.service("games");
 
   service.hooks(hooks);
 }
